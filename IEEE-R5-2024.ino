71f53b3da6e9a73dac5ca0de1810c631d90501e1
Minh Nguyen <45446622+samminhch@users.noreply.github.com>
2023-10-26 20:33:48 -0500

    removed slider support, motor control only uses potentiometers

--- IEEE-R5-2024.ino
+++ IEEE-R5-2024.ino
@@ -7,14 +7,11 @@ const motor right_motor{6, 7, 8};
 const int pot_left  = A2;
 const int pot_right = A1;
 
-const int slide_pin = 2;
-
 void setup() {
     // motor setup
     setup_motor(left_motor);
     setup_motor(right_motor);
 
-    pinMode(slide_pin, INPUT);
     Serial.begin(9600);
 }
 
@@ -22,17 +19,18 @@ void loop() {
     int pot_left_val  = analogRead(pot_left);
     int pot_right_val = analogRead(pot_right);
 
-    int left_speed  = map(pot_left_val, 0, 1023, 0, 255);
-    int right_speed = map(pot_right_val, 0, 1023, 0, 255);
-    int slide_val   = digitalRead(slide_pin);
+    int left_speed  = map(pot_left_val, 0, 1023, MOTOR_MIN, MOTOR_MAX);
+    int right_speed = map(pot_right_val, 0, 1023, MOTOR_MIN, MOTOR_MAX);
 
     // set motor speed + direction
-    spin_motor(left_motor, left_speed, (bool) slide_val);
-    spin_motor(right_motor, right_speed, (bool) slide_val);
+    spin_motor(left_motor, left_speed);
+    spin_motor(right_motor, right_speed);
 
     // print to serial monitor
     char buffer[STR_LEN];
-    sprintf(buffer, "Left_Motor_Speed:%d,Right_Motor_Speed:%d,Slider_Value:%d",
-            left_speed, right_speed, slide_val);
+    sprintf(buffer, "Left_Motor_Speed:%d,Right_Motor_Speed:%d",
+            left_speed, right_speed);
     Serial.println(buffer);
+    // spin_motor(left_motor, 255);
+    // spin_motor(right_motor, 255);
 }
