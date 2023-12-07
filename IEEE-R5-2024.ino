ecc15ec4c2f9d9c1e8b0a4b7aeadbe850d291034
samminhch@stemtop <45446622+samminhch@users.noreply.github.com>
2023-12-07 17:28:24 -0600

    code for paths

--- IEEE-R5-2024.ino
+++ IEEE-R5-2024.ino
@@ -1,36 +1,41 @@
 #include "robot-movement.h"
-#define STR_LEN 64
+#define STR_LEN   64
+#define NUM_PATHS 8
 
-const motor left_motor{11, 10, 9};
-const motor right_motor{6, 7, 8};
+struct path
+{
+        const double distance;
+        const double angles[2];
+};
 
-const int pot_left  = A2;
-const int pot_right = A1;
+const path paths[] = {
+    {.distance = 101.76, .angles = {45, -135} }, // A ➡️ D
+    {.distance = 107.28, .angles = {-27, -153}}, // D ➡️ H
+    {.distance = 75.84,  .angles = {-71, -161}}, // H ➡️ F
+    {.distance = 107.28, .angles = {-63, -153}}, // F ➡️ B
+    {.distance = 101.76, .angles = {-45, 135} }, // B ➡️ G
+    {.distance = 75.84,  .angles = {-18, -108}}, // G ➡️ E
+    {.distance = 75.84,  .angles = {-18, -108}}, // E ➡️ C
+    {.distance = 75.84,  .angles = {-18, -108}}, // C ➡️ A
+};
+int index = 0;
+
+motor left_motor{5, 4, 3, 2};
+motor right_motor{6, 7, 8, 9};
 
 void setup()
 {
     // motor setup
     setup_motor(left_motor);
     setup_motor(right_motor);
-
-    Serial.begin(9600);
 }
 
 void loop()
 {
-    int pot_left_val  = analogRead(pot_left);
-    int pot_right_val = analogRead(pot_right);
-
-    int left_speed  = map(pot_left_val, 0, 1023, MOTOR_MIN, MOTOR_MAX);
-    int right_speed = map(pot_right_val, 0, 1023, MOTOR_MIN, MOTOR_MAX);
-
-    // set motor speed + direction
-    spin_motor(left_motor, left_speed);
-    spin_motor(right_motor, right_speed);
-
-    // print to serial monitor
-    char buffer[STR_LEN];
-    sprintf(buffer, "Left_Motor_Speed:%d,Right_Motor_Speed:%d", left_speed,
-            right_speed);
-    Serial.println(buffer);
+    turn(paths[index].angles[0]);
+    move(paths[index].distance, left_motor, right_motor);
+    turn(paths[index].angles[1]);
+    index = (index + 1) % NUM_PATHS;
 }
+
+
