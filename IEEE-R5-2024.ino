dd2e4ee999172185551ad8963bfae6a607724580
samminhch@stemtop <45446622+samminhch@users.noreply.github.com>
2024-01-22 16:19:50 -0600

    added code to test encoder count

--- IEEE-R5-2024.ino
+++ IEEE-R5-2024.ino
@@ -1,8 +1,8 @@
 #include "robot-movement.h"
 #define STR_LEN 64
 
-const motor left_motor{11, 10, 9};
-const motor right_motor{6, 7, 8};
+motor left_motor{5, 4, 3, 2};
+motor right_motor{6, 7, 8, 9};
 
 const int pot_left  = A2;
 const int pot_right = A1;
@@ -13,24 +13,35 @@ void setup()
     setup_motor(left_motor);
     setup_motor(right_motor);
 
+    attachInterrupt(digitalPinToInterrupt(left_motor.encoder_pin), update_left_encoder, RISING);
+    attachInterrupt(digitalPinToInterrupt(right_motor.encoder_pin), update_right_encoder, RISING);
+
     Serial.begin(9600);
 }
 
 void loop()
 {
-    int pot_left_val  = analogRead(pot_left);
-    int pot_right_val = analogRead(pot_right);
-
-    int left_speed  = map(pot_left_val, 0, 1023, MOTOR_MIN, MOTOR_MAX);
-    int right_speed = map(pot_right_val, 0, 1023, MOTOR_MIN, MOTOR_MAX);
-
     // set motor speed + direction
-    spin_motor(left_motor, left_speed);
-    spin_motor(right_motor, right_speed);
+    spin_motor(left_motor, 80);
+    spin_motor(right_motor, 80);
 
     // print to serial monitor
     char buffer[STR_LEN];
-    sprintf(buffer, "Left_Motor_Speed:%d,Right_Motor_Speed:%d", left_speed,
-            right_speed);
-    Serial.println(buffer);
+    sprintf_P(buffer, "left_count:%d,right_count:%d\n", left_motor.encoder_count, right_motor.encoder_count);
+
+    delay(2500);
+}
+
+void update_left_encoder()
+{
+    pinMode(left_motor.encoder_pin, INPUT);
+    left_motor.encoder_count += digitalRead(left_motor.forward_dir_pin) ? 1 : -1;
+    pinMode(left_motor.encoder_pin, OUTPUT);
+}
+
+void update_right_encoder()
+{
+    pinMode(right_motor.encoder_pin, INPUT);
+    right_motor.encoder_count += digitalRead(right_motor.forward_dir_pin) ? 1 : -1;
+    pinMode(right_motor.encoder_pin, OUTPUT);
 }
